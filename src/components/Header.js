9b5d1e9f61b67d8e1e237179a1cc7f94454a7a2d
mehmet ali büber <mehmetali_buber@hotmail.com>
2021-02-27 23:10:19 +0300

    db

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Button from "./Button";
-const Header = ({title , showAdd, onAdd}) => {
+import { useLocation } from 'react-router-dom'
 
+const Header = ({title , showAdd, onAdd}) => {
+    const location = useLocation()
     return (
         <header className="header">
         <h1> {title}</h1>
-        <Button color={showAdd ? "red" : "green"} text={showAdd ? "close" : "add"} onClick={onAdd}/>
+        {location.pathname === '/' && (
+        <Button
+          color={showAdd ? 'red' : 'green'}
+          text={showAdd ? 'Close' : 'Add'}
+          onClick={onAdd}
+        />
+      )}
         </header>
     )
 }
